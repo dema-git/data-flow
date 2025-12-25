46a114ee8d20495096985fc8a84b5c9d43c52052
dema-git <git.dema@gmail.com>
2025-12-25 19:40:26 +0100

    change: comments in airflow DAG files Ref #11

--- init-db/01-create-airflow-db.sql
+++ init-db/01-create-airflow-db.sql
@@ -1,3 +1,8 @@
+-- ###########################################################
+-- 01-create-airflow-db.sql
+-- Initialization script for PostgreSQL database for Apache Airflow
+-- ################################################################
+
 CREATE USER airflow WITH PASSWORD 'airflow';
 CREATE DATABASE airflow_db OWNER airflow;
 GRANT ALL PRIVILEGES ON DATABASE airflow_db TO airflow;
