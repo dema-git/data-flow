21398c909a28da081182249ed2c3d80480995694
dema-git <git.dema@gmail.com>
2025-12-29 21:04:29 +0100

    add: try/except for all integrations endpoints Ref #23

--- fast-api/api/integrations_routes.py
+++ fast-api/api/integrations_routes.py
@@ -18,12 +18,13 @@
 # duplicate/inconsistent records. !!!
 ###########################################################################################################
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from kafka_consumer import start_consumer_loop, get_messages
 from minio_utils.files import move_files_to_another_bucket, get_files_data
 from db_utils.helpers import process_records
 from db_utils.database import get_db_session
 from models import engine
+from exceptions_logging.custom_exceptions import MinIOException, KafkaException
 
 router = APIRouter(tags=["Integrations"])
 
@@ -38,12 +39,19 @@ start_consumer_loop()
     This endpoint is primarily triggered by an Airflow DAG as part of the data pipeline.
     Manual invocation should be performed **only in exceptional cases**, such as debugging or recovery.
     
-    ⚠️ Manual execution may affect pipeline consistency.
+    WARNING: Manual execution may affect pipeline consistency.
     """
 )
 def kafka_consumer():
-    batch = get_messages()
-    return {"status": "success", "batch": batch}
+    try:
+        batch = get_messages()
+        return {"status": "success", "batch": batch}
+    except MinIOException as e:
+        raise HTTPException(status_code=500, detail=e.message)
+    except KafkaException as e:
+        raise HTTPException(status_code=500, detail=e.message)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.get(
@@ -55,14 +63,21 @@ def kafka_consumer():
     This endpoint is primarily triggered by an Airflow DAG as part of the data pipeline.
     Manual invocation should be performed **only in exceptional cases**, such as debugging or recovery.
     
-    ⚠️ Manual execution may affect pipeline consistency.
+    WARNING: Manual execution may affect pipeline consistency.
     """
 )
 def get_files_from_minio_bucket():
-    files_data = get_files_data('active-bucket')
-    with get_db_session() as db:
-        skipped = process_records(db, files_data)
-    return {"status": "done", "skipped_sessions": skipped}
+    try:
+        files_data = get_files_data('active-bucket')
+        with get_db_session() as db:
+            skipped = process_records(db, files_data)
+        return {"status": "done", "skipped_sessions": skipped}
+    except MinIOException as e:
+        raise HTTPException(status_code=e.status_code, detail=e.message)
+    except KafkaException as e:
+        raise HTTPException(status_code=e.status_code, detail=e.message)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.get(
@@ -73,9 +88,16 @@ def get_files_from_minio_bucket():
     This endpoint is primarily triggered by an Airflow DAG as part of the data pipeline.
     Manual invocation should be performed **only in exceptional cases**, such as debugging or recovery.
     
-    ⚠️ Manual execution may affect pipeline consistency.
+    WARNING: Manual execution may affect pipeline consistency.
     """
 )
 def move_all_files_from_primary_to_archive_bucket():
-    move_files_to_another_bucket('active-bucket', 'archive-bucket')
-    return {"status": "success"}
+    try:
+        move_files_to_another_bucket('active-bucket', 'archive-bucket')
+        return {"status": "success"}
+    except MinIOException as e:
+        raise HTTPException(status_code=e.status_code, detail=e.message)
+    except KafkaException as e:
+        raise HTTPException(status_code=e.status_code, detail=e.message)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
