ec422e58593298cef57e3070d6c8f432c29da46f
dema-git <git.dema@gmail.com>
2025-12-30 11:34:34 +0100

    set up: logger.py file Ref #16

---
+++ fast-api/exceptions_logging/logger.py
@@ -0,0 +1,42 @@
+####################################################################
+# logger.py
+# Returns a logger that writes to <LEVEL>.log (INFO.log, WARNING.log, ERROR.log)
+####################################################################
+
+import logging
+import os
+from typing import Union
+
+
+LOG_DIR = "/shared/logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+def get_logger(level: Union[int, str] = logging.INFO):
+    """
+    Returns a logger that writes to <LEVEL>.log (INFO.log,
+    WARNING.log, ERROR.log)
+    """
+    # Check if level is 'str'
+    if isinstance(level, str):
+        level = logging.getLevelName(level.upper())
+
+    level_name = logging.getLevelName(level)
+    log_file = os.path.join(LOG_DIR, f"{level_name}.log")
+
+    logger = logging.getLogger(level_name)
+    logger.setLevel(level)
+
+    if not logger.handlers:
+        handler = logging.FileHandler(log_file)
+        handler.setLevel(level)
+
+        formatter = logging.Formatter(
+            "%(asctime)s | %(levelname)s | %(message)s",
+            datefmt="%H:%M:%S",
+        )
+        handler.setFormatter(formatter)
+
+        logger.addHandler(handler)
+        logger.propagate = False
+
+    return logger
