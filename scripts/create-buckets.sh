6a3b4c2936504f7dd32f52c8adaa32787995b801
dema-git <git.dema@gmail.com>
2026-02-01 21:02:04 +0100

    add: all minio buckets to create-buckets.sh Ref #43

--- scripts/create-buckets.sh
+++ scripts/create-buckets.sh
@@ -6,8 +6,23 @@ sleep 5
 
 mc alias set local http://minio:9010 "$MINIO_USER" "$MINIO_PASSWORD"
 
-mc mb --ignore-existing local/bronze
-mc mb --ignore-existing local/silver
-mc mb --ignore-existing local/gold
+# BRONZE (RAW), active + archive buckets
+mc mb --ignore-existing local/events-bronze
+mc mb --ignore-existing local/events-bronze-archive
+
+
+# SILVER (cleaned layer), active + archive buckets
+mc mb --ignore-existing local/events-silver
+mc mb --ignore-existing local/events-silver-archive
+
+
+# GOLD — Page Views, active + archive buckets
+mc mb --ignore-existing local/events-gold-page-views
+mc mb --ignore-existing local/events-gold-page-views-archive
+
+
+# GOLD — Product Events, active + archive buckets
+mc mb --ignore-existing local/events-gold-product-events
+mc mb --ignore-existing local/events-gold-product-events-archive
 
 echo "Buckets created!"
